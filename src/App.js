38c8b43a37c0964e0fddd1bc221f6c7ccc2a9359
stevenhao <stevenhao@gmail.com>
2018-09-14 23:32:09 -0700

    add dropzone

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dropzone from 'react-dropzone';
 
 class App extends Component {
+  handleDrop = (files) => {
+    const file = files[0]
+    if (!file) {
+      throw new Error('No file uploaded :(')
+    }
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const result = e.target.result
+      console.log(result)
+      // TODO generate screenId, then save in localStorage, then redirect / render a link to /:screenId/1
+    }
+    reader.readAsDataURL(file)
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,10 +26,13 @@ class App extends Component {
           <h1 className="App-title">Welcome to Steven's House</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, upload a screenshot of your wallpaper.
+          {/* TODO add instructions explaining how to screenshot your wallpaper, and emphasize that must be a **screenshot**, not the original photo */}
         </p>
+
+        <Dropzone onDrop={this.handleDrop}/>
       </div>
-    );
+    )
   }
 }
 
