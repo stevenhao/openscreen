13f993d0cd122b0a391ae071f6a05329f6310050
stevenhao <stevenhao@gmail.com>
2018-09-14 22:30:54 -0700

    set up react router

---
+++ src/Screen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Screen extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello this is not steven's house
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Screen from './Screen';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Switch, IndexRoute, Link } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Router>
+    <Switch>
+      <Route path="/:screenId/:iconIndex" component={Screen} />
+      <Route path="/" component={App}>
+        {/*<IndexRoute component={Home} />*/}
+      </Route>
+    </Switch>
+  </Router>
+), document.getElementById('root'))
+
 registerServiceWorker();
