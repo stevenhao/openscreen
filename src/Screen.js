e30fe65057c4b26b009121c27745ea6a01df08c5
stevenhao <stevenhao@gmail.com>
2018-09-14 22:35:36 -0700

    extract route param info

--- src/Screen.js
+++ src/Screen.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 export default class Screen extends React.Component {
+  get screenId() {
+    const { screenId } = this.props.match.params
+    return screenId
+  }
+
+  get iconIndex() {
+    const { iconIndex } = this.props.match.params
+    return parseInt(iconIndex, 10)
+  }
+
+
+  renderDebugInfo() {
+    return (
+      <div>
+        <div>Screen Id: {this.screenId}</div>
+        <div>Icon Index: {this.iconIndex}</div>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
         Hello this is not steven's house
+        {this.renderDebugInfo()}
       </div>
     )
   }
