9d025f13fdf9228a95be50da988643bb00a8c1d8
stevenhao <stevenhao@gmail.com>
2018-09-14 23:09:35 -0700

    add react-helmet to experiment

--- src/Screen.js
+++ src/Screen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 export default class Screen extends React.Component {
   get screenId() {
@@ -11,6 +12,9 @@ export default class Screen extends React.Component {
     return parseInt(iconIndex, 10)
   }
 
+  get iconCount() {
+    return 16
+  }
 
   renderDebugInfo() {
     return (
@@ -21,9 +25,20 @@ export default class Screen extends React.Component {
     )
   }
 
+  renderHelmet() {
+    return (
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Icon {`${this.iconIndex}`} of {`${this.iconCount}`}</title>
+        <link rel="apple-touch-icon" href="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjVw-f3q7zdAhViOX0KHdW9CPEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.facebook.com%2FPusheen%2F&psig=AOvVaw1PpcnzkWZxiE2I53hkj29G&ust=1537078009279375" />
+      </Helmet>
+    )
+  }
+
   render() {
     return (
       <div>
+        {this.renderHelmet()}
         Hello this is not steven's house
         {this.renderDebugInfo()}
       </div>
